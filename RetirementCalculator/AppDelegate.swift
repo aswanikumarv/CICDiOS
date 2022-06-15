d111150d5cc29e86ebf723bb0b04700c2ffedde3
aswanikumarv <aswanikumar_v@outlook.com>
2022-06-15 12:23:43 +0530

    Integrated Appcenter

--- RetirementCalculator/AppDelegate.swift
+++ RetirementCalculator/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        AppCenter.start(withAppSecret: "1e543867-9376-4b77-91d2-169d4cffb300", services: [Analytics.self, Crashes.self])
         return true
     }
 
